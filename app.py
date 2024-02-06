18074f47fe17a0388eba795c36c5c8df87e2863e
michellechung099 <mchung099@gmail.com>
2024-02-06 10:27:24 -0800

    initial project structure

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# first parameter: alls code() when you visit root URL 
+# methods parameter: restricts which request methods the view allows
 @app.route("/", methods=["GET"])
+
+# view function that you prepend with app.route() decorators
+# entry point of the web app 
 def code():
+
     pass 
